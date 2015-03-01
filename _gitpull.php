7bc3b9d388ba1b6275df9f9e41bbd82dc2974f69
Fery Wardiyanto <ferywardiyanto@gmail.com>
2015-03-02 02:49:46 +0700

    Update _gitpull.php

--- _gitpull.php
+++ _gitpull.php
@@ -4,7 +4,7 @@ if (isset($_SERVER['HTTP_X_GITHUB_EVENT'])) {
 	`git pull`;
 
 	http_response_code(202);
-	echo 'Success!';
+	echo 'Deployment '.$_SERVER['HTTP_X_GITHUB_DELIVERY'].' Success!';
 } else {
 	http_response_code(403);
 	echo 'Not allowed!';
