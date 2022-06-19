ebe7360e84277000f8607b9f060c6fd3393bcec4
akrindev <akrinmin@gmail.com>
2022-06-19 22:36:37 +0700

    add cta

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -13,6 +13,32 @@
           Merupakan sebuah perusahaan yang bergerak dalam bidang jasa
           perancangan dan pengembangan perangkat lunak.
         </p>
+
+        <!-- cta -->
+        <div class="mt-6">
+          <a
+            href="#"
+            class="py-5 flex items-center text-primary hover:underline font-bold"
+            target="_blank"
+          >
+            <span class="mr-2"> Jelajahi Proyek </span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="feather feather-arrow-right"
+            >
+              <line x1="5" y1="12" x2="19" y2="12"></line>
+              <polyline points="12 5 19 12 12 19"></polyline>
+            </svg>
+          </a>
+        </div>
       </div>
     </div>
 
