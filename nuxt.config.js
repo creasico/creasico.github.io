2758bafaa8ba49233e08d685921ff8eb13a0bf52
Fery Wardiyanto <ferywardiyanto@gmail.com>
2022-06-21 06:27:27 +0700

    Reinit favicons images
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,4 +8,17 @@ export default defineNuxtConfig({
     },
   },
   css: ["~/assets/css/tailwind.css"],
+  head: {
+    meta: [
+      { hid: 'description', name: 'description', content: 'Meta description' },
+      { name: 'msapplication-TileColor', content: '#da532c' },
+      { name: 'theme-color', content: '#ffffff' },
+    ],
+    link: [
+      { rel: "icon", sizes: "32x32", type: "image/png", href: "/favicon-32x32.png" },
+      { rel: "icon", sizes: "16x16", type: "image/png", href: "/favicon-16x16.png" },
+      { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png" },
+      { rel: "manifest", href: "/site.webmanifest" },
+    ]
+  }
 });
