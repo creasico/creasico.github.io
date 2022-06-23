f01d7ae95c006c5dc50e57a045374ec1c6575a7d
Fery Wardiyanto <ferywardiyanto@gmail.com>
2022-06-24 02:07:50 +0700

    Use tailwind's built-in nesting plugin.
    See: https://tailwindcss.com/docs/using-with-preprocessors\#nesting
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   plugins: [
-    require('postcss-nested'),
+    require('postcss-import'),
+    require('tailwindcss/nesting'),
     require('tailwindcss'),
     require('autoprefixer'),
   ],
