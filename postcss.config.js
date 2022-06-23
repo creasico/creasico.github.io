977d80c4fd7ad01f5661ecbd3def6f5684ff68b4
Fery Wardiyanto <ferywardiyanto@gmail.com>
2022-06-24 01:33:42 +0700

    Add postcss-nested plugin
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [
+    require('postcss-nested'),
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ],
 }
