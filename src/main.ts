15bb3d6730ac78e16b7506bda060b36841a584cc
Fery Wardiyanto <ferywardiyanto@gmail.com>
2022-06-28 13:50:33 +0700

    chore: rearrange stylesheets
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { ViteSSG } from 'vite-ssg'
 import autoRoutes from 'pages-generated'
 import { setupLayouts } from 'layouts-generated'
 
-import './assets/css/tailwind.css'
+import './assets/styles/main.css'
 import App from './app.vue'
 
 // https://github.com/antfu/vite-ssg
