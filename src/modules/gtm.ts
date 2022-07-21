2c5ff41e50129b87a29614861ff30db341a8f8c7
Fery Wardiyanto <ferywardiyanto@gmail.com>
2022-07-21 19:57:30 +0700

    Enable debug mode of vue-gtm for development env
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- src/modules/gtm.ts
+++ src/modules/gtm.ts
@@ -3,22 +3,20 @@ import type { UserModule } from '~/types'
 
 // https://github.com/antfu/vite-plugin-pwa#automatic-reload-when-new-content-available
 export const install: UserModule = ({ app, isClient, router, routes }) => {
-  if (!isClient || !import.meta.env.VITE_GTM_ID)
+  if (!isClient && !import.meta.env.VITE_GTM_ID)
     return
 
   const gtm = createGtm({
     id: import.meta.env.VITE_GTM_ID,
     vueRouter: router,
+    debug: import.meta.env.DEV,
   })
 
   if (import.meta.env.DEV)
     console.log(routes) // eslint-disable-line no-console
 
   router.afterEach((to) => {
-    const tag = useGtm()
-
-    // console.log(to)
-    tag?.trackView(to.name as string, to.fullPath)
+    useGtm()?.trackView(to.name as string, to.fullPath)
   })
 
   app.use(gtm)
