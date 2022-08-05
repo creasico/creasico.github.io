73cdb44bdf40db0a8a7ffdc147a6ba02e006e370
Fery Wardiyanto <ferywardiyanto@gmail.com>
2022-08-06 03:48:16 +0700

    chore: update type declarations
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- src/shim.d.ts
+++ src/shim.d.ts
@@ -1,9 +1,16 @@
+import type { FirebaseOptions } from 'firebase/app'
+import { AppLocale } from './types'
+
 export {}
+
+declare global {
+  const FIREBASE_CONFIG: FirebaseOptions
+}
 
 declare module 'vue-router' {
   interface RouteMeta {
     layout?: string
-    locale?: 'id' | 'en',
+    locale?: AppLocale
     title?: string
     description?: string
   }
--- src/types.ts
+++ src/types.ts
@@ -4,9 +4,11 @@ import type { Frontmatter } from 'vite-plugin-md'
 
 export type UserModule = (ctx: ViteSSGContext) => void
 
+export type AppLocale = 'id' | 'en'
+
 export interface UserFrontmatter extends Frontmatter {
   container?: 'wide' | 'narrow'
   layout?: string
-  locale?: 'id' | 'en'
+  locale?: AppLocale
   showCTA?: boolean
 }
