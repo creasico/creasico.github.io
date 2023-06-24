8398ead15da4b342c531ec64c3c128cfa6f33097
Fery Wardiyanto <ferywardiyanto@gmail.com>
2023-06-24 19:35:26 +0700

    chore: workaround with type definitions
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,5 @@
 /// <reference lib="DOM" />
-/// <reference types="vite/client" />
-/// <reference types="vue/ref-macros" />
+/// <reference types="vitest" />
 /// <reference types="vite-plugin-pwa/client" />
 /// <reference types="vite-plugin-pages/client" />
 /// <reference types="vite-plugin-vue-layouts/client" />
