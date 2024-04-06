dfc9b47711c6b583a2d03dbd05d70c849c754c33
Fery Wardiyanto <ferywardiyanto@gmail.com>
2024-04-06 21:31:00 +0700

    chore: patch type definitions
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,22 +1,27 @@
 /// <reference lib="DOM" />
-/// <reference types="vitest" />
+/// <reference types="@intlify/unplugin-vue-i18n/messages" />
+/// <reference types="unplugin-vue-router/client" />
 /// <reference types="vite/client" />
-/// <reference types="vue/ref-macros" />
 /// <reference types="vite-plugin-pwa/client" />
 /// <reference types="vite-plugin-vue-layouts/client" />
-/// <reference types="@intlify/unplugin-vue-i18n/messages" />
+/// <reference types="vitest" />
+/// <reference types="vue/ref-macros" />
 
 interface ImportMetaEnv {
   VITE_GTM_ID: string
 }
 
-declare module '*.vue' {
+declare interface Window {
+  // extend the window
+}
+
+declare module '*.md' {
   import type { ComponentOptions } from 'vue'
   const Component: ComponentOptions
   export default Component
 }
 
-declare module '*.md' {
+declare module '*.vue' {
   import type { ComponentOptions } from 'vue'
   const Component: ComponentOptions
   export default Component
