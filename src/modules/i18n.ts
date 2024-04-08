973c2206b979d6e84d15183ed0bd63c410bd56b6
Fery Wardiyanto <ferywardiyanto@gmail.com>
2024-04-08 22:42:51 +0700

    chore: rely on site preference to set default locale
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- src/modules/i18n.ts
+++ src/modules/i18n.ts
@@ -1,12 +1,11 @@
 import { createI18n } from 'vue-i18n'
 import messages from '@intlify/unplugin-vue-i18n/messages'
-
-import { type UserModule } from '~/types'
+import type { UserModule } from '~/types'
 
 export const install: UserModule = ({ app }) => {
   const i18n = createI18n({
     legacy: false,
-    locale: 'en',
+    locale: sitePreference.value.locale,
     fallbackLocale: 'id',
     messages,
   })
